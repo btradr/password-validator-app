ed6a331bce05d95ed521c157c44bdfa23593917f
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 22:44:34 +0200

    feat(variables): created variables

---
+++ src/app/shared/variables.ts
@@ -0,0 +1,3 @@
+export const simplePattern: RegExp = /^[a-zA-Z]*$|^[0-9]*$|^[\\!@#$%^&*)(/+=.,_-]*$/;
+export const mediumPattern: RegExp = /^[a-zA-Z0-9]*$|^[a-zA-Z\\!@#$%^&*)(/+=.,_-]*$|^[0-9\\!@#$%^&*)(/+=.,_-]*$/;
+export const strongPattern: RegExp = /^[a-zA-Z0-9\\!@#$%^&*)(/+=.,_-]*$/;
