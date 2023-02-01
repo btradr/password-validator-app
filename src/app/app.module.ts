58666c3eff51730ed65e2809c869c4a910ed049d
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 20:32:18 +0200

    feat(login): create login-form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
+import { LoginModule } from "./login/login.module";
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    LoginModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
