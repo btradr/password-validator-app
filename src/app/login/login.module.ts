5196e79cee362d67b6bdafc9d042929ad0092f06
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 20:51:47 +0200

    feat(validation): added template for validation

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 // Components
 import { LoginComponent } from './components/login/login.component';
 
+// Modules
+import { ValidationModule } from '../validation/validation.module';
+
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ValidationModule
+  ],
   exports: [LoginComponent]
 })
 export class LoginModule { }
