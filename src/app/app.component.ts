9f0b6df9a8761c8387d248b5195bd9316bf91a75
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 20:47:15 +0200

    feat(login): added template for login-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``
+  template: `
+    <app-login></app-login>
+  `
 })
 export class AppComponent { }
