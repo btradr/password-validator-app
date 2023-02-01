2c4c9edc4a1d274c76cf7828872969f2b392cb93
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 22:47:50 +0200

    feat(validity): added logic for validity component

--- src/app/validation/components/validation/validation.component.ts
+++ src/app/validation/components/validation/validation.component.ts
@@ -1,20 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
-import { ControlError } from '../../../shared/interfaces/control-error.type';
+import { simplePattern, mediumPattern, strongPattern } from '../../../shared/variables';
+import { PasswordStrengthType } from '../../../shared/interfaces/password-strength.type';
+import { ControlErrorType } from '../../../shared/interfaces/control-error.type';
 
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.scss']
 })
-export class ValidationComponent implements OnInit {
+export class ValidationComponent implements OnInit, OnDestroy {
 
   @Input() public control: AbstractControl;
-  public controlError: ControlError = null;
+
+  public controlError: ControlErrorType = null;
+  public passwordStrength: PasswordStrengthType = null;
+
+  private destroy$: Subject<null> = new Subject<null>();
 
   public ngOnInit(): void {
     this.controlError = this.control.errors;
+    this.checkMatches();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  private checkMatches(): void {
+    this.control.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((inputValue: string) => {
+        if (inputValue.length <= 8) {
+          return;
+        }
+
+        if (inputValue.match(simplePattern)) {
+          this.passwordStrength = 'simple';
+        } else if (inputValue.match(mediumPattern)) {
+          this.passwordStrength = 'medium';
+        } else if (inputValue.match(strongPattern)) {
+          this.passwordStrength = 'strong';
+        } else {
+          this.passwordStrength = null;
+        }
+      });
   }
 
 }
