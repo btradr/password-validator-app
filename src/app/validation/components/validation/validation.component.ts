4dcd5d0faf25b26a0c1c6dca39b22b5b30c3b08a
Andrew Baturin <baturin.andrew1501@gmail.com>
2023-02-01 21:57:26 +0200

    feat(validation): added logic for validation component

--- src/app/validation/components/validation/validation.component.ts
+++ src/app/validation/components/validation/validation.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+import { ControlError } from '../../../shared/interfaces/control-error.type';
 
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.scss']
 })
-export class ValidationComponent { }
+export class ValidationComponent implements OnInit {
+
+  @Input() public control: AbstractControl;
+  public controlError: ControlError = null;
+
+  public ngOnInit(): void {
+    this.controlError = this.control.errors;
+  }
+
+}
